e8ea8d2233e5f7dad38ca16c2543b6d05004804a
Antonnn95 <34644124+Antonnn95@users.noreply.github.com>
2021-09-23 09:52:40 +0200

    Nu är jag klar på riktigt

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav id="mainNav">
+    <nav>
       <router-link id="Home" to="/">Home</router-link>
       <router-link id="searchLink" to="/search">Search</router-link>
     </nav>
